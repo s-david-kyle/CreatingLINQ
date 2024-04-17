a8e11ab696a4af442770109399e57d63ad34409a
S. David Kyle <sdavidkyle@gmail.com>
2024-04-16 17:18:31 -0700

    Add iteration over IEnumerable in Main method

--- Program.cs
+++ Program.cs
@@ -2,8 +2,32 @@
 
 class Program
 {
+
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IEnumerable<int> e = GetValues();
+        using IEnumerator<int> enumerator = e.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            int i = enumerator.Current;
+            Console.WriteLine(i);
+        }
+
+
+        foreach (int i in GetValues())
+        {
+            Console.WriteLine(i);
+
+        }
+
+
+        static IEnumerable<int> GetValues()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
     }
 }
