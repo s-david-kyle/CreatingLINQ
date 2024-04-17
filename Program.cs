db9b1231f2b6d78bd026ca80edb7391bb6da7982
S. David Kyle <sdavidkyle@gmail.com>
2024-04-16 18:04:13 -0700

    Refactor iteration over IEnumerable in Main method and add Person class

--- Program.cs
+++ Program.cs
@@ -3,26 +3,28 @@
 class Program
 {
 
+    IEnumerable<Person> people = new List<Person>
+    {
+        new Person { Name = "John" },
+        new Person { Name = "Jane" },
+        new Person { Name = "Jack" }
+    };
 
+    IEnumerable<string> names = people.Select(p => p.Name);
 
     static void Main(string[] args)
     {
-        IEnumerable<int> e = GetValues();
-        IEnumerator<int> enumerator = e.GetEnumerator();
-        Console.WriteLine(enumerator);
-        while (enumerator.MoveNext())
-        {
-            Console.WriteLine(enumerator.Current);
-            int i = enumerator.Current;
-            Console.WriteLine(i);
-        }
-
-
-        static IEnumerable<int> GetValues()
+        static IEnumerable<TResult> Select<TSource, TResult>(IEnumeable<TSource> source)
         {
             yield return 1;
             yield return 2;
             yield return 3;
         }
     }
+}
+
+class Person
+{
+    public string Name { get; set; }
+
 }
